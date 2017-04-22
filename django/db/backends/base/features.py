79ea7562e5f612c99f7be672e0279a5145a1459f
Tom <tom@tomforb.es>
2017-04-22 16:44:51 +0100

    Add a supports_aggregate_filter_clause database feature

--- django/db/backends/base/features.py
+++ django/db/backends/base/features.py
@@ -227,6 +227,9 @@ class BaseDatabaseFeatures:
     supports_select_difference = True
     supports_slicing_ordering_in_compound = False
 
+    # Supports SQL 2003 FILTER (WHERE ...) in aggregate expressions
+    supports_aggregate_filter_clause = False
+
     # Does the backend support indexing a TextField?
     supports_index_on_text_field = True
 
