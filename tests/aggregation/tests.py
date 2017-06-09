377b25a5a6385ab75728699ba33ca3f728b223a6
Simon Charette <charette.s@gmail.com>
2017-06-09 01:04:51 -0400

    Added total ordering to a test to make it pass.

--- tests/aggregation/tests.py
+++ tests/aggregation/tests.py
@@ -61,7 +61,7 @@ class FilteredAggregateTestCase(TestCase):
 
     def test_plain_annotate(self):
         agg = Sum('book__pages', filter=Q(book__rating__gt=3))
-        qs = Author.objects.annotate(pages=agg)
+        qs = Author.objects.annotate(pages=agg).order_by('pk')
         self.assertSequenceEqual([a.pages for a in qs], [447, None])
 
     def test_filtered_aggregate_on_annotate(self):
