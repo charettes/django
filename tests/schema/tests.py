e9274aa30953ac348c68d3b2b5410417698ba12b
Simon Charette <charette.s@gmail.com>
2021-02-07 21:21:50 -0500

    Refs #32425 -- Added regression test.

--- tests/schema/tests.py
+++ tests/schema/tests.py
@@ -3095,6 +3095,30 @@ class SchemaTests(TransactionTestCase):
             if connection.features.can_introspect_default:
                 self.assertIsNone(field.default)
 
+    def test_add_field_default_nullable(self):
+        # Create the table
+        with connection.schema_editor() as editor:
+            editor.create_model(Author)
+        # Add new nullable CharField with a default
+        new_field = CharField(max_length=15, blank=True, null=True, default='surname')
+        new_field.set_attributes_from_name("surname")
+        with connection.schema_editor() as editor:
+            editor.add_field(Author, new_field)
+        Author.objects.create(name='Anonymous1')
+        # Ensure field is still nullable
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT surname FROM schema_author;")
+            item = cursor.fetchall()[0]
+            self.assertIsNone(item[0])
+            field = next(
+                f for f in connection.introspection.get_table_description(cursor, "schema_author")
+                if f.name == "surname"
+            )
+            self.assertIs(True, field.null_ok)
+            # And that the default is no longer set in the database.
+            if connection.features.can_introspect_default:
+                self.assertIsNone(field.default)
+
     def test_alter_field_default_dropped(self):
         # Create the table
         with connection.schema_editor() as editor:
